546721ad1a75bbd3522fa11ec55d4038db91c4b5
unknown <adehasanudin1@gmail.com>
2020-01-21 05:36:00 +0700

    portal berita Android Studio

---
+++ app/src/main/java/com/adha/portalberitaadha/network/InitRetrofit.java
@@ -0,0 +1,19 @@
+package com.adha.portalberitaadha.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class InitRetrofit {
+    // URL Server API
+    public static String API_URL = "https://adha-belajar.000webhostapp.com/portalberita/";
+
+    public static Retrofit setInit() {
+        return new Retrofit.Builder().baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static ApiServices getInstance() {
+        return setInit().create(ApiServices.class);
+    }
+}
